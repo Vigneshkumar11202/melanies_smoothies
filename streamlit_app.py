d39e0ada92153488be3903e04e102d2819f90a29
Spider VK <61825210+Vigneshkumar11202@users.noreply.github.com>
2026-01-11 17:20:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ ingredient_list=st.multiselect(
     max_selections=5
 )
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/")
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/all")
 #st.text(smoothiefroot_response.json())
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True )
 
