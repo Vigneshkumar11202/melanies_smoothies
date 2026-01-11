07bb5d0034376801333d67ac3360e42d20707b22
Spider VK <61825210+Vigneshkumar11202@users.noreply.github.com>
2026-01-11 16:54:47 +0530

    Update streamlit_app.py
    Make a Version of my_dataframe, but call it pd_df

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas
 
 # Write directly to the app
 st.title(f"Customize Your Smoothie :cup_with_straw:")
@@ -26,6 +27,11 @@ cnx=st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+
+#convert snowpark dataframe to pandas dataframe so we can use the LOC function
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 ingredient_list=st.multiselect(
     "Choose up to 5 ingredients",
